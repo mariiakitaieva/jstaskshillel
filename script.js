ee6e1192c03390d9752e0c9f42e660e8b0330bca
MariiaKit <kitaievam@ukr.net>
2024-01-25 05:31:43 +0200

    task2changes

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 //alert("Hello, World!");
-userName = prompt("Enter your name: ");
-console.log("Hello, ", userName, "! How are you?");
+//userName = prompt("Enter your name: ");
+//console.log("Hello, ", userName, "! How are you?");
+
+//# зробити конвертер з градусів Цельсія у градуси Фаренгейту
+//- (C * (9/5)) + 32 = F
+
+const celciusDegrees= +prompt("Enter Celsius degrees:");
+const fahrenheitDegrees = celciusDegrees*9/5+32;
+document.write(`${celciusDegrees}°C is ${fahrenheitDegrees}°F`);
+console.log(`${celciusDegrees}°C is ${fahrenheitDegrees}°F`);
