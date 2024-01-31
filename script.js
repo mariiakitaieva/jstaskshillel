96e2f3aba5a17f0e796d0ff8c25e583f778db4f0
MariiaKit <kitaievam@ukr.net>
2024-01-31 22:52:56 +0200

    task 3

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-//alert("Hello, World!");
-//userName = prompt("Enter your name: ");
-//console.log("Hello, ", userName, "! How are you?");
+//Користувач вводить через prompt номер пальця на руці.
+// Вивести в alert назву пальця, відповідно до його номеру (якщо 1, то великий; якщо 2, то вказівний і тд)
 
-//# зробити конвертер з градусів Цельсія у градуси Фаренгейту
-//- (C * (9/5)) + 32 = F
-
-const celciusDegrees= +prompt("Enter Celsius degrees:");
-const fahrenheitDegrees = celciusDegrees*9/5+32;
-document.write(`${celciusDegrees}°C is ${fahrenheitDegrees}°F`);
-console.log(`${celciusDegrees}°C is ${fahrenheitDegrees}°F`);
+const fingerNumber = Number(prompt("Enter the number of the finger from 1 to 5: "))
+if (fingerNumber ===1){
+    alert("Thumb finger")}
+else if(fingerNumber===2){
+    alert("Index finger")}
+else if(fingerNumber===3){
+    alert("Middle finger")}
+else if(fingerNumber===4){
+    alert("Ring finger")}
+else if(fingerNumber===5){
+    alert("Pinky finger")}
+else {alert("You entered wrong data")}
