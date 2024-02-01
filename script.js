444d1151716227f20b6dad64583b3a26e2e46b0d
MariiaKit <kitaievam@ukr.net>
2024-02-01 22:33:28 +0200

    task4 LeapYear

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-//Користувач вводить через prompt номер пальця на руці.
-// Вивести в alert назву пальця, відповідно до його номеру (якщо 1, то великий; якщо 2, то вказівний і тд)
+//Визначити, чи є рік високосним, чи ні. Рік буде високосним, якщо він кратний 400
+//або він одночасно кратний 4 і не кратний 100 ﻿Рік вводиться через prompt, результат - в алерт.
 
-const fingerNumber = Number(prompt("Enter the number of the finger from 1 to 5: "))
-if (fingerNumber ===1){
-    alert("Thumb finger")}
-else if(fingerNumber===2){
-    alert("Index finger")}
-else if(fingerNumber===3){
-    alert("Middle finger")}
-else if(fingerNumber===4){
-    alert("Ring finger")}
-else if(fingerNumber===5){
-    alert("Pinky finger")}
-else {alert("You entered wrong data")}
+const year = +prompt("Enter the year: ")
+
+if ((year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0)) { 
+    alert("The year is a Leap Year!")
+}
+else{
+    alert("The year is not a Leap Year!")
+}
