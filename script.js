d6c29a84411b2135b6a2a8401469910d265a66f6
MariiaKit <kitaievam@ukr.net>
2024-02-03 22:22:05 +0200

    task5

--- script.js
+++ script.js
@@ -1,11 +1,72 @@
-//Визначити, чи є рік високосним, чи ні. Рік буде високосним, якщо він кратний 400
-//або він одночасно кратний 4 і не кратний 100 ﻿Рік вводиться через prompt, результат - в алерт.
+//Вирахувати факторіал числа за допомогою for або while. 
+//Число вводиться з промпту, число знаходиться в межах від 1 до 50.
+// Факторіал - це добуток натуральних чисел від одиниці до вашого числа, наприклад факторіал від 7 буде:
+//1 * 2 * 3 * 4 * 5 * 6 * 7 = 5040
+//або від 5
+//1 * 2 * 3 * 4 * 5 = 120
 
-const year = +prompt("Enter the year: ")
+//за допомогою масива і циклу for
+// let startNumber = Number(prompt("Enter the number in the range between 1 and 50: "))
 
-if ((year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0)) { 
-    alert("The year is a Leap Year!")
+// let newArr = []
+// let factorial = 1
+
+// for (let i = 0; i < startNumber; i++){
+//     newArr.push(i+1)
+//     console.log(newArr)
+//     factorial *= newArr[i]
+// }
+
+// console.log("factorial=", factorial)
+
+
+//----------------------------------------------------------------------------------------------
+//за допомогою циклу for
+// //n! = 1*2*3*4*5*6*7*8*9*10
+// //n! = n * (n - 1) * (n - 2) * ...*1
+
+let startNumber = Number(prompt("Enter the number in the range between 1 and 50: "))
+while (startNumber < 1 || startNumber > 50){
+    startNumber = Number(prompt("Enter the number in the range between 1 and 50: "))}
+
+
+let factorial = 1
+for( let i = 0; i < startNumber; i++){
+    factorial = factorial * (startNumber - i)
+    if (i === startNumber - 1){
+        console.log("factorial = ", factorial)}
 }
-else{
-    alert("The year is not a Leap Year!")
-}
+
+
+
+
+//-----------------------------------------------------------------------------------------------
+//за допомогою циклу do while
+// let startNumber = Number(prompt("Enter the number in the range between 1 and 50: "))
+//while (startNumber <1 || startNumber>50){
+//    startNumber = Number(prompt("Enter the number in the range between 1 and 50: "))}
+
+// let factorial = 1
+// let period = 0
+
+// do {factorial = factorial * (startNumber - period)
+// period += 1
+// } while (period < startNumber);
+
+// console.log(factorial)
+
+
+//за допомогою циклу while
+// let startNumber = Number(prompt("Enter the number in the range between 1 and 50: "))
+//while (startNumber <1 || startNumber>50){
+//    startNumber = Number(prompt("Enter the number in the range between 1 and 50: "))}
+
+// let factorial = 1
+// let period = 0
+
+// while (period < startNumber){
+//     factorial = factorial * (startNumber - period)
+//     period += 1
+// }
+
+// console.log(factorial)
